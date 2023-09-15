bbd9e1ce702a56f1759675cbc328e1361e56acc0
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-15 16:55:33 +0200

    Add
    to do milestone 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,15 @@ messaggi relativi al contatto attivo all’interno del pannello della conversazi
 ● Click sul contatto mostra la conversazione del contatto cliccato
 
 
-Milestone 3
+Milestone 3 ✅
 ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
 “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 
+Milestone 4
+● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+“mar” rimangono solo Marco e Martina
 
 */
 

b51e839ed2d627932fb0e15662e9005736a447d1
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-18 12:02:31 +0200

    Add
    fixing chat bubbles  (in progress)

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,15 @@ Milestone 3 ✅
 “enter” il testo viene aggiunto al thread sopra, come messaggio verde
 ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
 
-Milestone 4
+Milestone 4 ✅
 ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
 contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
 “mar” rimangono solo Marco e Martina
+
+Milestone 5 - opzionale
+● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che
+permette di cancellare il messaggio selezionato
+● Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
 
 */
 
